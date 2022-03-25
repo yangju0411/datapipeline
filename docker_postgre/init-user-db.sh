09ef40fe2e7452ee0f16d02c1f06a4d93db83ff3
Helicobacto <yangju0411@gmail.com>
2022-03-25 16:39:02 +0900

    airflow 기본 세팅

--- docker_postgre/init-user-db.sh
+++ docker_postgre/init-user-db.sh
@@ -1,7 +1,7 @@
 set -e
 psql -v --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER test with ENCRYPTED password 'test';
+    CREATE USER airflow with ENCRYPTED password '0000';
     CREATE DATABASE airflow;
-    GRANT ALL PRIVILEGES ON DATABASE airflow TO test;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO test;
+    GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO airflow;
 EOSQL
