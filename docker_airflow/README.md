035a6773717be52fda4cea73df9eb6216f43123d
Helicobacto <yangju0411@gmail.com>
2022-03-26 22:59:18 +0900

    db 계정 수정 및 폴더에 readme 작성

---
+++ docker_airflow/README.md
@@ -0,0 +1,3 @@
+# airflow 컨테이너 관련 폴더
+
+- airflow.cfg: airflow config 파일
--- docker_postgre/init-user-db.sh
+++ docker_postgre/init-user-db.sh
@@ -1,7 +1,8 @@
 set -e
 psql -v --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER airflow with ENCRYPTED password '0000';
+    CREATE USER airflow with ENCRYPTED password 'airflow';
     CREATE DATABASE airflow;
     GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO airflow;
 EOSQL
+
